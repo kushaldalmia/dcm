0f96e6d12b6637d0da1a0bd9164ce8af2e309b0e
Kushal Dalmia <kdpict@gmail.com>
2012-03-18 18:15:03 -0400

    Implemented Heartbeating

--- nwmgr.py
+++ nwmgr.py
@@ -11,7 +11,7 @@ class nwManager:
         self.neighbors = {}
         self.conn = {}
         for n in neighborList:
-            self.neighbors[n] = 0
+            self.neighbors[n] = (0,0)
             self.conn[n] = createConn(n)
         self.available = []
         self.port = localPort
@@ -19,6 +19,7 @@ class nwManager:
         self.nodeId = 8
         self.seqno = 1
         self.ttl = 32
+        self.heartbeatTime = 0.0
 
     def createNewMessage(self, msgType, data):
         msg = str(self.nodeId) + "-" + str(self.seqno) + "-" + str(self.ttl) + "-" + msgType + "-" + data
@@ -30,31 +31,66 @@ class nwManager:
             self.conn[key].send(msg)
 
     def startManager(self):
+        curTime = time.time()
         initMsg = self.createNewMessage("NEIGHBOR_INIT", (self.localIP + ":" + str(self.port)))
         self.sendToNeighbors(initMsg)
+        for key in self.neighbors:
+            self.neighbors[key] = (curTime, 0)
+        self.heartbeatTime = curTime
+        
+        # Initalize listening socket
         server = socket(AF_INET, SOCK_STREAM)
+        server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         server.bind(('', self.port))
         server.listen(5)
+        server.settimeout(1)
+
+        # Main Server Loop
         while True:
-            client, addr = server.accept()
-            self.handleMessage(client.recv(4096))
+            try:
+                client, addr = server.accept()
+                self.handleMessage(client.recv(4096))
+            except:
+                pass
+            self.handleTimeouts()
+            self.sendHeartBeats()            
         
     def handleMessage(self, msgStr):
         print "Received : " + msgStr
         msg = Message(msgStr)
+        curTime = time.time()
         if msg.type == "NEIGHBOR_INIT":
-            self.neighbors[msg.data] = 0
+            self.neighbors[msg.data] = (curTime, 0)
             self.conn[msg.data] = createConn(msg.data)
             print "Added new node to neighbor " + msg.data
         elif msg.type == "HEARTBEAT":
-            # Handle HeartBeat Message
-            pass
+            self.neighbors[msg.data] = (curTime, 0)
         elif msg.type == "RES_AVL":
             # Handle Resource Available Message
             pass
         elif msg.type == "RES_UNAVL":
             # Handle Resource Unavailable Message
             pass
+    
+    def handleTimeouts(self):
+        curTime = time.time()
+        for n in self.neighbors:
+            timestamp, count = self.neighbors[n]
+            if curTime - timestamp > 10.0:
+                if count == 2:
+                    print "Node " + n + "Failed!"
+                    # Send RES_UNAVL if resource was available
+                else:
+                    print "No Heartbeat from " + n
+                    neighbors[n] = (curTime, count + 1)
+            
+    def sendHeartBeats(self):
+        curTime = time.time()
+        if curTime - self.heartbeatTime > 10.0:
+            hbtMsg = self.createNewMessage("HEARTBEAT", (self.localIP + ":" + str(self.port)))
+            self.sendToNeighbors(hbtMsg)
+            self.heartbeatTime = curTime
+        
 
 # Helper Routines
 def createConn(n):
